142a6227a75de1e300b8b05794a46b610cad0186
Vladislav <grt.cvb@mail.ru>
2023-10-26 20:17:02 +1000

    Added changes into ReaderTest
    Added message checking in exceptions
    Added one more test

--- src/test/java/org/kaoden/ws/homework/service/ReaderTest.java
+++ src/test/java/org/kaoden/ws/homework/service/ReaderTest.java
@@ -1,10 +1,8 @@
 package org.kaoden.ws.homework.service;
 
-import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import org.junit.jupiter.api.Test;
 import org.kaoden.ws.homework.obj.Entry;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -19,13 +17,17 @@ class ReaderTest {
     void invalidFilePath() {
         // Arrange
         String filePath = "test.json";
+        String expectedMessage = "File with this location \""+filePath+"\" doesn't exist";
+
+        // Act
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> reader.readEntriesFromJson(filePath));
 
         // Asset
-        assertThrows(RuntimeException.class, () -> reader.readEntriesFromJson(filePath));
+        assertEquals(expectedMessage, exception.getMessage());
     }
 
     @Test
-    void emptyDataFile() throws IOException {
+    void emptyDataFile() {
         // Arrange
         String filePath = "src\\test\\resources\\emptyData.json";
         Map<UUID, Entry> expectedEntries = new HashMap<>();
@@ -41,13 +43,17 @@ class ReaderTest {
     void invalidKeyNameInDataFile() {
         // Arrange
         String filePath = "src\\test\\resources\\dataWithInvalidKeyName.json";
+        String expectedMessage = "An error occurred while reading file: " + filePath;
+
+        // Act
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> reader.readEntriesFromJson(filePath));
 
         // Assert
-        assertThrows(UnrecognizedPropertyException.class, () -> reader.readEntriesFromJson(filePath));
+        assertEquals(expectedMessage, exception.getMessage());
     }
 
     @Test
-    void someFieldsMissingInDataFile() throws IOException {
+    void someFieldsMissingInDataFile() {
         String filePath = "src\\test\\resources\\dataWithMissingFields.json";
 
         // Act
@@ -60,7 +66,22 @@ class ReaderTest {
                 () -> assertEquals("Описание полезного материала", actualEntries.get(null).getDescription()),
                 () -> assertEquals("https://example.com/material", actualEntries.get(null).getLink())
         );
-
     }
 
+    @Test
+    void readEntryWithoutAllFields() {
+        // Arrange
+        String filePath = "src\\test\\resources\\dataWithEntryWithoutAllFields.json";
+
+        // Act
+        Map<UUID, Entry> actualEntries = reader.readEntriesFromJson(filePath);
+
+        // Assert
+        assertAll(
+                () -> assertNull(actualEntries.get(null).getUuid()),
+                () -> assertNull(actualEntries.get(null).getName()),
+                () -> assertNull(actualEntries.get(null).getDescription()),
+                () -> assertNull(actualEntries.get(null).getLink())
+        );
+    }
 }
